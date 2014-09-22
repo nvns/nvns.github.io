c9cad343a78c048febae73eb14e2f423c90218a5
Mikko <mikko@lookout.com>
2014-09-21 22:36:24 -0700

    Content generated from mikkot/nvns@a0c892d

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
 
   var showNavTargetDiv = function(htag) {
-    console.log("Showing nav target "+ htag);
-    // show active tab
+    // mark current li element as active
     var li = $('#navbar a[href=' + htag + ']').first().parent();
-    console.log("Activating nav tab "+ $(li).html());
     $(li).toggleClass('active', true);
+
+    // mark sibling li elements as not active
     $(li).siblings().each(function(i, sibling) {
-      console.log("Deactivating nav tab "+ $(sibling).html());
       $(sibling).toggleClass('active', false);
     });
 
     // hide all but requested div.nav-target on the page
     var div = $('div' + htag + '_target.nav-target').toggleClass('hidden', false);
-    console.log('Showing ' + $(div).attr('id'));
     $(div).siblings('.nav-target').each(function(i, div) {
-      console.log('Hiding ' + $(div).attr('id'));
       $(div).toggleClass('hidden', true);
+    });
+
+    // contact page is special, need to reload
+    // the GMap iFrame when it's first shown to 
+    // make it display correctly
+    if ('#contact' === htag) {
+      // yes it's just resetting the same URL which reloads the iframe
+      document.getElementById('gmap').src = document.getElementById('gmap').src;
+    }
+  }
+
+  var setupNavbarListeners = function() {
+    // then attach a click listener that displays the
+    // div matching nav item being clicked
+    $('#navbar li').each(function(i, li) {
+      $(li).click(function(event) {
+        // the post-hashtag part of the URL assigned to first <a/> child
+        // of the current list element under #nav
+        var tag = $(event.currentTarget).children('a').first().attr('href').split('#').pop();
+        showNavTargetDiv('#' + tag);
+        $('.collapse').collapse('hide');
+      });
     });
   }
 
-  // first, on the initial page load, display the
-  // div matching current URL
-  var htag = window.location.hash || '#index';
-  showNavTargetDiv(htag);
+  var showCurrentTargetDiv = function() {
+    // first, on the initial page load, display the
+    // div matching current URL
+    var htag = window.location.hash || '#index';
+    showNavTargetDiv(htag);
+  }
 
-  // then attach a click listener that displays the
-  // div matching nav item being clicked
-  $('#navbar li').each(function(i, li) {
-    $(li).click(function(event) {
-      // the post-hashtag part of the URL assigned to first <a/> child
-      // of the current list element under #nav
-      var tag = $(event.currentTarget).children('a').first().attr('href').split('#').pop();
-      showNavTargetDiv('#' + tag);
-      $('.collapse').collapse('hide');
-    });
-  });
+  setupNavbarListeners();
+  showCurrentTargetDiv();
 });
