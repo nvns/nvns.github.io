ccc0c2c91d05c521ccab74c2ce38a31ed9ed579c
Chris Yap <buzzyapyear@gmail.com>
2017-02-26 21:53:21 -0800

    fine-tuning scroll threshold for showing header background

--- _assets/js/controllers/DefaultPage.js
+++ _assets/js/controllers/DefaultPage.js
@@ -3,17 +3,20 @@ nvns.nvnsApp.controller('DefaultPageCtrl', ['$scope', '$timeout',
                                      function($scope, $timeout){
 
     $scope.vars = {
-        scrolled: false
+        scrolled: false,
+        viewport_width: 0
     }
 
     $scope.init = function() {
+
+        $scope.getViewportWidth();
+
+        $(window).resize(function(){
+            $scope.vars.viewport_width = $scope.getViewportWidth();
+        });
+
         $(window).scroll(function(){
-            var scroll = $(window).scrollTop();
-            $timeout(function(){
-                $scope.$apply(function(){
-                    $scope.vars.scrolled = (scroll > 100) ? true : false;
-                });
-            });
+            $scope.setScrolledState();
         });
 
         (function(d, s, id) {
@@ -23,6 +26,21 @@ nvns.nvnsApp.controller('DefaultPageCtrl', ['$scope', '$timeout',
             js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
+    }
+
+    $scope.setScrolledState = function() {
+        var scroll = $(window).scrollTop(),
+            threshold = ($scope.vars.viewport_width >= 1199) ? 100 : 0;
+
+        $timeout(function(){
+            $scope.$apply(function(){
+                $scope.vars.scrolled = (scroll > threshold) ? true : false;
+            });
+        });
+    }
+
+    $scope.getViewportWidth = function() {
+        return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     }
 
     window.scope = $scope;
