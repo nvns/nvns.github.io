2e688bf43ae80042a707107cfe83a0a8b48879ab
Christopher Yap <buzzyapyear@gmail.com>
2018-09-07 11:16:42 -0700

    make video playback more robust by checking for player state on an interval and forcing play

--- _assets/js/controllers/Home.js
+++ _assets/js/controllers/Home.js
@@ -1,8 +1,14 @@
+nvns.nvnsApp.controller('HomeCtrl', HomeCtrl);
 
-nvns.nvnsApp.controller('HomeCtrl', ['$scope', '$timeout',
-                                     function($scope, $timeout){
+HomeCtrl.$inject = [
+    '$scope',
+    '$timeout',
+    '$interval'
+];
 
-    $scope.vars = {
+function HomeCtrl ($scope, $timeout, $interval) {
+
+    var sv  = $scope.vars = {
         scrolled: false,
         video_ready: false,
         video: {
@@ -20,49 +26,103 @@ nvns.nvnsApp.controller('HomeCtrl', ['$scope', '$timeout',
                 origin: window.location.href
             },
             muted: true
-        }
+        },
+        video_check_interval: null
     }
 
+    /**
+     * @name $scope.init
+     * @function
+     * @memberOf HomeCtrl
+     * @description Initialize the app
+     */
     $scope.init = function() {
 
         $(window).scroll(function(){
             var scroll = $(window).scrollTop();
             $timeout(function(){
                 $scope.$apply(function(){
-                    $scope.vars.scrolled = (scroll > 0) ? true : false;
+                    sv.scrolled = (scroll > 0) ? true : false;
                 });
             });
         });
 
-        $scope.initVideoPlayer();
+        initVideoPlayer();
 
     }
 
-    $scope.initVideoPlayer = function() {
-        if ($scope.vars.video.player) {
-            /*$scope.$on('youtube.player.ready', function(event, player){
-                $scope.vars.video.player.mute();
-            });*/
+    /**
+     * @name initVideoPlayer
+     * @function
+     * @memberOf HomeCtrl
+     * @description This method starts the youtube video player for our
+     *              video backdrop.
+     */
+    function initVideoPlayer() {
+        if (sv.video.player) {
             $scope.$on('youtube.player.playing', function(event, player){
-                $scope.vars.video_ready = true;
+                sv.video_ready = true;
             });
+            $timeout(function() {
+                if (!sv.video_check_interval) {
+                    sv.video_check_interval = $interval(ensureVideoIsPlaying, 10000);
+                }
+            }, 10000);
+
         }
         else {
-            $timeout($scope.initVideoPlayer, 10);
+            $timeout(initVideoPlayer, 10);
         }
     }
 
+    /**
+     * @name ensureVideoIsPlaying
+     * @function
+     * @memberOf HomeCtrl
+     * @description Check video status, if it's stopped, make sure it starts again
+     */
+    function ensureVideoIsPlaying() {
+        /*
+         * Sometimes the youtube player stops or enters an unexpected state
+         * for reasons unknown, possibly having to do with the machine having
+         * been put to sleep or the browser tab throttled for some reason.
+         * This regular check every ten seconds attempts to ensure the video
+         * resumes playing if this happens.  ~cyap
+         */
+        if (sv.video.player) {
+            var player_status = sv.video.player.getPlayerState();
+            if (!isNaN(player_status)) {
+                if ([-1, 0, 2, 5].indexOf(player_status)) {
+                    // status codes represent:
+                    // -1 unstarted
+                    // 0 stopped
+                    // 2 paused
+                    // 5 video cued
+                    sv.video.player.mute();
+                    sv.video.player.playVideo();
+                }
+            }
+        }
+    }
+
+    /**
+     * @name $scope.toggleVideoSound
+     * @function
+     * @memberOf HomeCtrl
+     * @description This method allows the user to turn on the audio for the
+     *              background video.
+     */
     $scope.toggleVideoSound = function() {
-        if ($scope.vars.video.muted) {
-            $scope.vars.video.player.unMute();
-            $scope.vars.video.muted = false;
+        if (sv.video.muted) {
+            sv.video.player.unMute();
+            sv.video.muted = false;
         }
         else {
-            $scope.vars.video.player.mute();
-            $scope.vars.video.muted = true;
+            sv.video.player.mute();
+            sv.video.muted = true;
         }
     }
 
     window.scope = $scope;
 
-}]);
+};
